9dbd4328c6e013fef6eaa41e8fd6a7d0aa4073d5
Alex Manthei <alex.manthei@deliveroo.co.uk>
2020-06-11 00:53:24 +0100

    Adds ingredients and tag components

---
+++ src/components/ingredients.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Tag from "./tag"
+
+const Ingredients = ({ ingredient }) => (
+  <div
+    style={{
+      margin: `0 auto`,
+      maxWidth: 960,
+      padding: `1.45rem 1.0875rem`,
+    }}
+  >
+    <ul style={{ margin: 0, listStyleType: `none` }}>
+      {ingredient.map(function (item) {
+        if (item[0] !== null) {
+          return (
+            <li key={item}>
+              <Tag text={item[0]} />
+              {item[1]}
+            </li>
+          )
+        } else {
+          return <li key={item}>{item[1]}</li>
+        }
+      })}
+    </ul>
+  </div>
+)
+
+export default Ingredients
